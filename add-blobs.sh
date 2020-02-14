612e43812a164bc960f0e03f1346a831ff8c8422
Diego Fidalgo, Deepak Mehta and Panos Xynos <notingithub>
2020-02-14 11:25:40 +0000

    Updated add_blobs.sh

--- add-blobs.sh
+++ add-blobs.sh
@@ -1,37 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-DIR=`pwd`
-
-mkdir -p .downloads
-
-cd .downloads
-
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DOWNLOAD_FOLDER="$THIS_SCRIPT_DIR/.downloads"
 ES_VERSION=7.5.0
 
-if [ ! -f ${DIR}/blobs/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz ];then
-    curl -L -J -o elasticsearch-${ES_VERSION}.tar.gz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-${ES_VERSION}-no-jdk-linux-x86_64.tar.gz
-    bosh add-blob --dir=${DIR} elasticsearch-${ES_VERSION}.tar.gz elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
-fi
+plugin_blob_url_list=(\
+    https://artifacts.elastic.co/downloads/elasticsearch-plugins/analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip \
+    https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-s3/repository-s3-${ES_VERSION}.zip \
+    https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-gcs/repository-gcs-${ES_VERSION}.zip \
+    https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-azure/repository-azure-${ES_VERSION}.zip \
+)
 
-if [ ! -f ${DIR}/blobs/analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip
-    bosh add-blob --dir=${DIR} analysis-kuromoji-${ES_VERSION}.zip analysis-kuromoji/analysis-kuromoji-${ES_VERSION}.zip
-fi
+mkdir -p $DOWNLOAD_FOLDER/{elasticsearch,elasticsearch-plugins}
 
-if [ ! -f ${DIR}/blobs/repository-s3/repository-s3-${ES_VERSION}.zip ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-s3/repository-s3-${ES_VERSION}.zip
-    bosh add-blob --dir=${DIR} repository-s3-${ES_VERSION}.zip repository-s3/repository-s3-${ES_VERSION}.zip
-fi
-
-if [ ! -f ${DIR}/blobs/repository-gcs/repository-gcs-${ES_VERSION}.zip ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-gcs/repository-gcs-${ES_VERSION}.zip
-    bosh add-blob --dir=${DIR} repository-gcs-${ES_VERSION}.zip repository-gcs/repository-gcs-${ES_VERSION}.zip
-fi
-
-if [ ! -f ${DIR}/blobs/repository-azure/repository-azure-${ES_VERSION}.zip ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch-plugins/repository-azure/repository-azure-${ES_VERSION}.zip
-    bosh add-blob --dir=${DIR} repository-azure-${ES_VERSION}.zip repository-azure/repository-azure-${ES_VERSION}.zip
-fi
-
-
-cd -
+pushd "$DOWNLOAD_FOLDER" 
+    pushd elasticsearch
+        curl -L -O -J https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}-no-jdk-linux-x86_64.tar.gz
+    popd
+    pushd elasticsearch-plugins
+        for blob_url in "${plugin_blob_url_list[@]}"; do
+            curl -L -O -J "$blob_url"
+        done
+    popd
+    for file in $(find . -type file); do
+        bosh add-blob --dir="$THIS_SCRIPT_DIR" ${file} ${file##\./}
+    done
+popd
