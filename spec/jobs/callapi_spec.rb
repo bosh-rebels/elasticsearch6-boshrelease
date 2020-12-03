c71a4680fcc9bf225c92c9857ff7c2bcec2cee51
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2020-12-03 10:22:27 +0000

    [wip] introducing callapi errand
    Signed-off-by: Panagiotis Xynos <panos@onlyascii.io>

---
+++ spec/jobs/callapi_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require 'yaml'
+require 'json'
+require 'bosh/template/test'
+
+describe 'callapi job' do
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
+  let(:job) { release.job('callapi') }
+
+  describe 'run.sh.erb' do
+    let(:template) { job.template('bin/run') }
+    let(:links) { [
+        Bosh::Template::Test::Link.new(
+          name: 'elasticsearch',
+          instances: [Bosh::Template::Test::LinkInstance.new(address: '10.0.8.2')],
+          properties: {
+            'elasticsearch'=> {
+              'cluster_name' => 'test'
+            },
+          }
+        )
+      ] }
+
+    describe 'providing a call' do
+      let(:valid_call) {
+        YAML.load(%q(
+        - endpoint: /_ilm/policy/datastream_policy
+          action: PUT
+          headers:
+            - Content-Type: application/json
+          payload: |
+            {
+              "policy": {
+                "phases": {
+                  "hot": {
+                    "min_age": "0ms",
+                    "actions": {
+                      "rollover": {
+                        "max_age": "1d"
+                      }
+                    }
+                  },
+                  "delete": {
+                    "min_age": "7d",
+                    "actions": {
+                      "delete": {}
+                    }
+                  }
+                }
+              }
+            }
+        ))
+      }
+
+      let(:valid_rendered_request) {
+        "request_elk PUT /_ilm/policy/datastream_policy"
+      }
+
+      let(:valid_rendered_payload) {
+        '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1d"}}},"delete":{"min_age":"7d","actions":{"delete":{}}}}}}'
+      }
+
+      it 'renders properly' do
+        expect { template.render({"calls" => valid_call}, consumes: links) }.not_to raise_error
+        rendered_template = template.render({"calls" => valid_call}, consumes: links)
+        expect(rendered_template).to include(valid_rendered_request)
+        # expect(rendered_template).to include(valid_rendered_payload.to_json.to_s)
+        puts rendered_template
+      end
+    end
+  end
+end
