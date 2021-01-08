7b7dd695415dec25f0eb9f41d5ca55a23550546d
Mehta Deepak <deepak.mehta@servexternos.gruposantander.com>
2021-01-08 14:16:14 +0000

    should support multiple header

--- spec/jobs/callapi_spec.rb
+++ spec/jobs/callapi_spec.rb
@@ -15,7 +15,10 @@ describe 'callapi job' do
           instances: [Bosh::Template::Test::LinkInstance.new(address: '10.0.8.2')],
           properties: {
             'elasticsearch'=> {
-              'cluster_name' => 'test'
+              'cluster_name' => 'test',
+              'client' => {
+                'port' => '9200'
+              }
             },
           }
         )
@@ -53,18 +56,18 @@ describe 'callapi job' do
       }
 
       let(:valid_rendered_request) {
-        "request_elk PUT /_ilm/policy/datastream_policy"
+        "curl -s -X  PUT -H 'Content-Type: application/json'  http://10.0.8.2:9200/_ilm/policy/datastream_policy"
       }
 
       let(:valid_rendered_payload) {
-        '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1d"}}},"delete":{"min_age":"7d","actions":{"delete":{}}}}}}'
+        'curl -s -X PUT -H \'Content-Type: application/json\' -k http://10.0.8.2:9200/_ilm/policy/datastream_policy -d {"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"1d"}}},"delete":{"min_age":"7d","actions":{"delete":{}}}}}}'
       }
 
       it 'renders properly' do
         expect { template.render({"calls" => valid_call}, consumes: links) }.not_to raise_error
         rendered_template = template.render({"calls" => valid_call}, consumes: links)
         expect(rendered_template).to include(valid_rendered_request)
-        # expect(rendered_template).to include(valid_rendered_payload.to_json.to_s)
+        #expect(rendered_template).to include(valid_rendered_payload.to_s)
         puts rendered_template
       end
     end
